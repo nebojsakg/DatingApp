d18d73d56ce0aac80a0a17b17dbf7149cbec3095
Nebojsa Nedeljkovic <nebojsa.nedeljkovic@nortal.com>
2019-07-08 09:08:09 +0200

    end of section 4

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -26,4 +26,13 @@ export class NavComponent implements OnInit {
     );
   }
 
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
 }
