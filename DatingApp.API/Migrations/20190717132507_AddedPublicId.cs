2de9385e7327b2351a07cb2b560f6d00a174bbc1
Nebojsa Nedeljkovic <nebojsa.nedeljkovic@nortal.com>
2019-07-17 15:52:55 +0200

    cloudinary setup and photo upload

---
+++ DatingApp.API/Migrations/20190717132507_AddedPublicId.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class AddedPublicId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DAteAdded",
+                table: "Photos",
+                newName: "DateAdded");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Photos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Photos");
+
+            migrationBuilder.RenameColumn(
+                name: "DateAdded",
+                table: "Photos",
+                newName: "DAteAdded");
+        }
+    }
+}
